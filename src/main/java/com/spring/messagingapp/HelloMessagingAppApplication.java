204fbf1979bc3c2c45625af9a2dda784416a34cd
saiteja-code <saitejakanaparthy@gmail.com>
2020-12-11 22:19:41 +0530

    Hello Message App

---
+++ src/main/java/com/spring/messagingapp/HelloMessagingAppApplication.java
@@ -0,0 +1,13 @@
+package com.spring.messagingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMessagingAppApplication {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Hello Messaging App");
+        SpringApplication.run(HelloMessagingAppApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/spring/messagingapp/controller/HelloWorldController.java
@@ -0,0 +1,36 @@
+package com.spring.messagingapp.controller;
+
+import com.spring.messagingapp.model.User;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+
+    // GET Request :- Rest call: curl localhost:8080/hello -w "\n"
+    @RequestMapping(value = {"", "/", "/home"})
+    public String hello(){
+        return "Hello from Bridgelabz";
+    }
+
+    // Query Parameter
+    // GET Request:- Rest call: curl localhost:8080/hello/query?name=Mark -w "\n"
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String hello(@RequestParam(value = "name") String name){
+        return "Hello " + name + " !!";
+    }
+
+    // Path Variable
+    // GET Request:- Rest call: curl localhost:8080/hello/param/Mark -w "\n"
+    @GetMapping("/param/{name}")
+    public String helloParam(@PathVariable String name){
+        return "Hello " + name +" !!";
+    }
+
+    // Post Request:- Rest call: curl -X POST -H "Content-Type: application/json" -d '{\"firstName\": \”Mark\", \"lastName\": \”Taylor\"}'
+    //                           "http://localhost:8080/hello/post" -w "\n"
+    @PostMapping("/post")
+    public String hello(@RequestBody User user){
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " !!";
+    }
+}
